53b501e39a84abc92813605e6e318699926154bd
PeterMa <jma4@ualberta.ca>
2020-12-03 18:08:15 -0700

    update api url

--- common/setting/Setting.js
+++ common/setting/Setting.js
@@ -1,7 +1,7 @@
 export default class Setting {
     static useHttp = true;
     static protocol = this.useHttp ? 'http://' : 'https://';
-    static baseUrl = this.protocol + 'localhost:3000/';
+    static baseUrl = this.protocol + '0.0.0.0:3000/';
     static apiBaseUrl = this.baseUrl + 'api/';
     static userApiUrl = this.apiBaseUrl + 'user/';
     static userLoginApiUrl = this.apiBaseUrl + 'user/login/';
